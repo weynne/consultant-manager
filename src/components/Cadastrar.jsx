2ef88ea649eda6cd1f36bcb0458beca02fc4d4ae
Isabel Gonçalves <igl@cesar.school>
2024-05-22 20:37:04 -0300

    commit cadastrar

--- src/components/Cadastrar.jsx
+++ src/components/Cadastrar.jsx
@@ -10,6 +10,12 @@ import {
   Select,
   TextField,
 } from '@mui/material';
+import { useConsultores } from '../context/consultores';
+import { postCidadeAPI } from '../services/cidades';
+import { postFormacoesAPI } from '../services/formacoes';
+import { postProfissaoAPI } from '../services/profissoes';
+import { postProjetosAPI } from '../services/projetos';
+import { postCatAPI } from '../services/cat';
 
 const Cadastrar = () => {
   const { profileData } = React.useContext(ProfileContext);
@@ -58,6 +64,241 @@ const Cadastrar = () => {
     'SE',
     'TO',
   ];
+
+  const { errors, handleBlur, handleInputChange, handleSubmit } =
+    useValidation();
+  const { postConsultor } = useConsultores();
+
+  const [combinedData, setCombinedData] = useState([]);
+  const [visibleText, setVisibleText] = useState(false);
+  const [selectValue, setSelectValue] = useState(formData.estado);
+  const [formacoes, setFormacoes] = useState([
+    { formacao: '', instituicao: '', tipo: '', anoConclusao: '' },
+  ]);
+  const [cats, setCats] = useState([{ descricao: '' }]);
+  const [projetos, setProjetos] = useState([{ nome: '' }]);
+  const [profissoes, setProfissoes] = useState([{ profissao: '', area: '' }]);
+
+  const addNewFormacao = () => {
+    setFormacoes([
+      ...formacoes,
+      { formacao: '', instituicao: '', tipo: '', anoConclusao: '' },
+    ]);
+  };
+  const addNewCat = () => {
+    setCats([...cats, { descricao: '' }]);
+  };
+  const addNewProjeto = () => {
+    setProjetos([...projetos, { nome: '' }]);
+  };
+  const addNewProfissao = () => {
+    setProfissoes([...profissoes, { profissao: '', area: '' }]);
+  };
+
+  const removeFormacao = (index) => {
+    if (formacoes.length > 1) {
+      const newFormacoes = formacoes.filter((_, i) => i !== index);
+      setFormacoes(newFormacoes);
+    }
+  };
+  const removeCat = (index) => {
+    if (cats.length > 1) {
+      const newCats = cats.filter((_, i) => i !== index);
+      setCats(newCats);
+    }
+  };
+  const removeProjeto = (index) => {
+    if (projetos.length > 1) {
+      const newProjetos = projetos.filter((_, i) => i !== index);
+      setProjetos(newProjetos);
+    }
+  };
+  const removeProfissao = (index) => {
+    if (profissoes.length > 1) {
+      const newProfissoes = profissoes.filter((_, i) => i !== index);
+      setProfissoes(newProfissoes);
+    }
+  };
+
+  const handleInfosInputChange = (index, field, value, type) => {
+    if (type === 'formacao') {
+      const newFormacoes = [...formacoes];
+      newFormacoes[index][field] = value;
+      setFormacoes(newFormacoes);
+    } else if (type === 'cat') {
+      const newCats = [...cats];
+      newCats[index][field] = value;
+      setCats(newCats);
+    } else if (type === 'projeto') {
+      const newProjetos = [...projetos];
+      newProjetos[index][field] = value;
+      setProjetos(newProjetos);
+    } else if (type === 'profissao') {
+      const newProfissoes = [...profissoes];
+      newProfissoes[index][field] = value;
+      setProfissoes(newProfissoes);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+    handleInputChange(name, value);
+  };
+  const handleBlurField = (event) => {
+    const { name, value } = event.target;
+    handleBlur(name, value);
+  };
+
+  const calcularIdade = (dataNascimento) => {
+    const hoje = new Date();
+    const nascimento = new Date(dataNascimento);
+    let idade = hoje.getFullYear() - nascimento.getFullYear();
+    const mes = hoje.getMonth() - nascimento.getMonth();
+    if (mes < 0 || (mes === 0 && hoje.getDate() < nascimento.getDate())) {
+      idade--;
+    }
+    return idade;
+  };
+  const calcularFormacao = (anoConclusao) => {
+    const hoje = new Date();
+    const formacao = new Date(anoConclusao);
+    let tempoFormacao = hoje.getFullYear() - formacao.getFullYear();
+    return tempoFormacao;
+  };
+
+  // ENVIO !! -------------------------------
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const idade = calcularIdade(formData.dataNascimento);
+
+    const dadosPessoais = {
+      id: 0,
+      cpf: formData.cpf,
+      cnpj: formData.cnpj,
+      nome: formData.nome,
+      telefone: formData.telefone,
+      email: formData.email,
+      dataNascimento: formData.dataNascimento,
+      idade: idade,
+    };
+
+    const dadosLocalizacao = {
+      id: 0,
+      cidade: {
+        nome: formData.cidade,
+        estado: {
+          uf: formData.estado,
+        },
+      },
+    };
+
+    const dadosAcademicos = {
+      formacoes: formacoes.map((formacao) => ({
+        id: 0,
+        nome: formacao.formacao,
+        instituicao: formacao.instituicao,
+        tipo: formacao.tipo,
+        anoConclusao: parseInt(formacao.anoConclusao, 10),
+        tempoFormacao: calcularFormacao(formacao.anoConclusao),
+      })),
+    };
+
+    const dadosProfissao = {
+      profissoes: profissoes.map((profissao) => ({
+        id: 0,
+        nome: profissao.area,
+        area: profissao.area,
+      })),
+    };
+
+    const dadosProjeto = {
+      projetos: projetos.map((projeto) => ({
+        id: 0,
+        nome: projeto.nome,
+      })),
+    };
+
+    const dadosCat = {
+      cat: cats.map((cat) => ({
+        id: 0,
+        descricao: cat.descricao,
+      })),
+    };
+
+    try {
+      if (handleSubmit(dadosPessoais)) {
+        await postConsultor(dadosPessoais);
+
+        await postCidadeAPI(dadosLocalizacao);
+
+        await postFormacoesAPI(dadosAcademicos);
+
+        await postProfissaoAPI(dadosProfissao);
+
+        await postProjetosAPI(dadosProjeto);
+
+        await postCatAPI(dadosCat);
+
+        console.log('Todos os dados foram enviados com sucesso!');
+        setCombinedData({
+          ...dadosPessoais,
+          ...dadosLocalizacao,
+          ...dadosAcademicosProfissionais,
+        });
+      }
+    } catch (error) {
+      console.error('Erro ao enviar os dados:', error);
+    }
+  };
+
+  // const onSubmit = (event) => {
+  //   event.preventDefault();
+
+  //   const idade = calcularIdade(formData.dataNascimento);
+
+  //   const dadosCombinados = {
+  //     cpf: formData.cpf,
+  //     cnpj: formData.cnpj,
+  //     nome: formData.nome,
+  //     telefone: formData.telefone,
+  //     email: formData.email,
+  //     dataNascimento: formData.dataNascimento,
+  //     idade: idade,
+  //     cidade: {
+  //       nome: formData.cidade,
+  //       estado: {
+  //         uf: formData.estado,
+  //       },
+  //     },
+  //     formacoes: formacoes.map((formacao) => ({
+  //       nome: formacao.formacao,
+  //       instituicao: formacao.instituicao,
+  //       tipo: formacao.tipo,
+  //       anoConclusao: parseInt(formacao.anoConclusao, 10),
+  //       tempoFormacao: calcularFormacao(formacao.anoConclusao),
+  //     })),
+  //     profissoes: profissoes.map((profissao) => ({
+  //       nome: profissao.profissao,
+  //       area: profissao.area,
+  //     })),
+  //     projetos: projetos.map((projeto) => ({
+  //       nome: projeto.nome,
+  //     })),
+  //     cat: cats.map((cat) => ({
+  //       descricao: cat.descricao,
+  //     })),
+  //   };
+
+  //   if (handleSubmit(dadosCombinados)) {
+  //     //       <--------------------Enviar os dados para o backend
+  //     console.log('Dados válidos:', dadosCombinados);
+  //     setCombinedData(dadosCombinados);
+  //     postConsultor(dadosCombinados);
+  //   }
+  // };
 
   return (
     <Container>
